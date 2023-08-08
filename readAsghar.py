33153a9d9b2329684c3594d0e342b942c54addb6
aliabdollahi024 <aliabdollahi024a@egmail.com>
2023-08-08 21:50:14 +0330

    Initial commit

---
+++ readAsghar.py
@@ -0,0 +1,5 @@
+import os
+my_varAsghar = os.environ['Asghar']
+my_varSectret = os.environ['Secret']
+print(my_varAsghar)
+print(my_varSectret)
---
+++ readMammad.py
@@ -0,0 +1,3 @@
+with open('mamad.txt') as f:
+    lines = f.readlines()
+print(lines[0])
