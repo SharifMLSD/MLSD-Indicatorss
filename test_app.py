2fa05583e6a2bcdbd04adf33fc1d0eca04b64494
n.hashemi202 <n.hashemi202@gmail.com>
2023-08-11 07:11:02 +0000

    Add new tests

--- test_app.py
+++ test_app.py
@@ -1,1 +1,18 @@
-print("Hi test app", '========================================')
+import requests
+import time
+
+def test_latency():
+    print("Testing ...")
+    start_time = time.time()
+    file_path = "vabemellat_30min_200.csv"
+    with open(file_path , "rb") as file:
+        files = {"file": file}
+        response = requests.post("https://mlsd-indicatorss.darkube.app/predict", files=files)
+    print("Response status: ", response.status_code, '=====================')
+    assert response.status_code == 200
+    end_time = time.time()
+    latency = end_time - start_time
+    print(f"Latency: {latency} seconds")
+
+if __name__=="__main__":
+    test_latency()
