7a066186cfdf6d11032d1f87fcfdc9cd85a3489a
n.hashemi202 <n.hashemi202@gmail.com>
2023-08-11 07:17:58 +0000

    Add tests for GET

--- test_app.py
+++ test_app.py
@@ -1,18 +1,23 @@
 import requests
 import time
 
-def test_latency():
-    print("Testing ...")
-    start_time = time.time()
-    file_path = "vabemellat_30min_200.csv"
+
+def test_get():
+    get_url = "https://mlsd-indicatorss.darkube.app/"
+    print("Testing GET method: ", get_url)
+    response = requests.get(get_url)
+    assert response.status_code == 200
+    
+
+def test_post():
+    post_url = "https://mlsd-indicatorss.darkube.app/predict"
+    print("Testing POST method: ", post_url)
+    file_path = "test_post_ci/vabemellat_30min_200.csv"
     with open(file_path , "rb") as file:
         files = {"file": file}
-        response = requests.post("https://mlsd-indicatorss.darkube.app/predict", files=files)
-    print("Response status: ", response.status_code, '=====================')
+        response = requests.post(post_url, files=files)
     assert response.status_code == 200
-    end_time = time.time()
-    latency = end_time - start_time
-    print(f"Latency: {latency} seconds")
+
 
 if __name__=="__main__":
-    test_latency()
+    test_get()
