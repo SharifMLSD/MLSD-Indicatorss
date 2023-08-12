aac6f85edae22fe6c618c5c9518780098f556f56
n.hashemi202 <n.hashemi202@gmail.com>
2023-08-12 15:11:41 +0000

    Debug label display

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def predict(file: UploadFile = File(...)):
     else:
         Metrics.pred_counter.labels(pred='POSITIVE').inc()
 
-    return {"label": if label_pred 'Buy!' else "Don't buy!"}
+    return {"label": 'Buy!' if label_pred else "Don't buy!"}
 
 
 if __name__ == "__main__":
